63dd82110614f3872834da8da8bd0584480688a0
= <=>
2024-03-05 22:27:27 -0500

    Added proper 'headers' for files, ready for A3 submission

--- controllers/mealkitController.js
+++ controllers/mealkitController.js
@@ -1,3 +1,14 @@
+/*************************************************************************************
+* WEB322 - 2241 Project
+* I declare that this assignment is my own work in accordance with the Seneca Academic
+* Policy. No part of this assignment has been copied manually or electronically from
+* any other source (including web sites) or distributed to other students.
+*
+* Student Name  : Emily Fagin
+* Student ID    : efagin (139498224)
+* Course/Section: WEB322/NEE
+*
+**************************************************************************************/
 const express = require("express");
 const mealkitUtil = require("../modules/mealkit-util");
 const router = express.Router();
